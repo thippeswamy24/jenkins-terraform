e7292652d4bd9ebde69b8e07384d4e0e2d4bc97b
thippeswamy24 <61265289+thippeswamy24@users.noreply.github.com>
2020-03-04 12:20:26 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "ap-south-1"
-  access_key = "AKIAUPSEJXY56X6HHBYU"
-  secret_key = "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"
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
 }
 resource "aws_instance" "new2" {
   ami           = "ami-0d9462a653c34dab7"

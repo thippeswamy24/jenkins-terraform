34cdbcbc4eab1ca0df78ff1d136f1f56073d9341
thippeswamy24 <61265289+thippeswamy24@users.noreply.github.com>
2020-03-04 10:24:53 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "ap-south-1"
 }
 resource "aws_instance" "new2" {
-  ami           = "ami-0217a85e28e625474"
+  ami           = "ami-0d9462a653c34dab7"
   instance_type = "t2.micro" 
 
   user_data     = <<-EOF

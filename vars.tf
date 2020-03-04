cd8508e59d5156bc83778cc6da609931e260dbb8
thippeswamy24 <61265289+thippeswamy24@users.noreply.github.com>
2020-03-04 12:23:37 +0530

    Create vars.tf

---
+++ vars.tf
@@ -0,0 +1,12 @@
+//you can either enter your access/secrect keys in here, or otherwise you can create a file with name terraform.tfvars with these values.
+
+
+variable "access_key" {
+    description = "Your AWS Access Key"
+    default = "AKIAUPSEJXY56X6HHBYU"
+}
+
+variable "secret_key" {
+    description = "Your AWS Secret Key"
+    default = "hFK+cERm+VcWTGz2IK/h5gJteKKGATy9Eq8d76Ed"
+}

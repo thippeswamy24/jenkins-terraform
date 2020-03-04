e98d7d34e9ae8d9303c94bbc77788a242b2dfa2a
thippeswamy24 <61265289+thippeswamy24@users.noreply.github.com>
2020-03-04 10:23:10 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,33 +1,15 @@
 provider "aws" {
-  region = "eu-west-2"
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
+  region = "ap-south-1"
+}
+resource "aws_instance" "new2" {
+  ami           = "ami-0217a85e28e625474"
+  instance_type = "t2.micro" 
+
+  user_data     = <<-EOF
+              #!/bin/bash
+              yum install nginx -y
+              yum update -y
+              EOF
+
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"]
-}
-
-resource "aws_instance" "web" {
-  ami           = "${data.aws_ami.ubuntu.id}"
-  instance_type = "t2.micro"
-
-  tags {
-    Name = "HelloWorld"
-  }
-}
-output "ip"{
-value= "${aws_instance.web.public_ip}"
-}
